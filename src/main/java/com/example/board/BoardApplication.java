ad24c484717a0c9db31fb3ffc3dc8318e052f275
gyocheol <kys04171@naver.com>
2025-07-09 18:42:30 +0900

    mysql connection completed

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,10 +2,12 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class BoardApplication {
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application, application-db");
         SpringApplication.run(BoardApplication.class, args);
     }
 }
