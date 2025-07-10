f51cdfc6f6c942770d5a2a8562396f4d61d5e238
gyocheol <kys04171@naver.com>
2025-07-10 17:40:40 +0900

    create entity

---
+++ src/main/java/com/example/board/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.board.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @OneToMany
+    private List<Board> boards = new ArrayList<>();
+}
